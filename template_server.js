ea27889a5d809f539298e06127c905f213478045
Jeffrey Shi <jeffrey.shi17@gmail.com>
2016-11-18 00:57:50 -0500

    big commit

--- template_server.js
+++ template_server.js
@@ -1,10 +1,34 @@
-var http = require('http');
+var express = require('express'),
+app = express(),
+http = require('http'),
+server = http.createServer(app),
+fs = require('fs'),
+ejs = require('ejs');
+
+var json_data_string = fs.readFileSync("data/concussiontracker.json", 'utf8');
+var PORT = 8080; 
+
+app.use(express.static('public'))
+
+app.set('view engine', 'ejs');
+app.get('/', function(req, res){
+        res.locals.data = JSON.parse(json_data_string);
+        res.render('index.ejs');
+});
+
+app.listen(PORT);
+console.log("Server working");
+
+/*var http = require('http');
 var fs = require('fs');
 var ejs = require('ejs');
+var express = require('express');
+var app = express();
 var server = http.createServer();
 server.on('request', function (req, res) {
-    var json_data_string = fs.readFileSync("data/dummy.json", 'utf8'); //synchronous file read
-    var json_data_object = JSON.parse(json_data_string);
+
+    var json_data_string = fs.readFileSync("data/concussiontracker.json", 'utf8'); //synchronous file read
+    var jsonObject = JSON.parse(json_data_string);
     fs.readFile("views/index.ejs", 'utf8', function (err, contents) {
         if (err) {
             console.log(err);
@@ -13,17 +37,16 @@ server.on('request', function (req, res) {
             });
             res.write("UH OH :(((((");
             res.end();
-        }
-        else {
+        } else {
             res.writeHead(200, {
                 'Content-Type': 'text/html'
             });
             var html = ejs.render(contents, {
-                data: json_data_object
+                data: jsonObject
             });
             res.write(html); //writes a dynamically-generated response body
             res.end();
         }
     }); //asynchronous file-read callback
-}); //on request callback
-server.listen(4000);
+});
+server.listen(4000);*/
